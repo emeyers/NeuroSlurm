c9ea2ac5522669ecc3feff68b0e666f7fa89f9c5
Ethan Meyers <em939@grace2.grace.hpc.yale.internal>
2023-05-01 14:59:01 -0400

    New jobs

--- results/r_markdown/analysis.sh
+++ results/r_markdown/analysis.sh
@@ -6,4 +6,4 @@
 #SBATCH --mem=1500G
 module load R/4.1.0-foss-2020b
 module load Pandoc/2.10
-Rscript -e "rmarkdown::render('Pending/NeuroShiny_Script_ID_20230326_161419_61744_em_try_02.Rmd')"
+Rscript -e "rmarkdown::render('Pending/NeuroShiny_Script_ID_20230326_161419_61744_em_try_03.Rmd')"
