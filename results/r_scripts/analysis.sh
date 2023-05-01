e6529b6694ba0c80d936491039f30097da570033
Ethan Meyers <em939@grace2.grace.hpc.yale.internal>
2023-05-01 14:06:02 -0400

    New jobs

--- results/r_scripts/analysis.sh
+++ results/r_scripts/analysis.sh
@@ -5,4 +5,4 @@
 #SBATCH --constraint cascadelake
 #SBATCH --mem=1500G
 module load R/4.1.0-foss-2020b
-Rscript Pending/"NeuroShiny_Script_ID_20230201_104106_07541.R"
+Rscript Pending/"test_william_run_ANOVA.R"
