5caaa5ae8e0ec596b72505248ebfd3265a601851
Ethan Meyers <em939@grace2.grace.hpc.yale.internal>
2023-05-01 15:08:01 -0400

    New jobs

--- results/r_scripts/Failed/failed_scripts/test_william_run_ANOVA_try_03.R
+++ results/r_scripts/Failed/failed_scripts/test_william_run_ANOVA_try_03.R
@@ -27,10 +27,9 @@ library(NeuroDecodeR)
 source("/home/em939/research/Allen_ANOVA_analyses/run_ANOVA.R")
 
 
-# binned_data_name <- "binned_data/Allen_715093703_10bins_10sampled.Rda"
+binned_data_name <- "binned_data/Allen_715093703_10bins_10sampled.Rda"
 #binned_data_name <- "binned_data/Allen_715093703_10bins_1sampled_start-100_end500.Rda"
-
-binned_data_name <- "/home/em939/research/Allen_ANOVA_analyses/binned_data/Allen_715093703_10bins_1sampled_start-50_end250.Rda"
+#binned_data_name <- "/home/em939/research/Allen_ANOVA_analyses/binned_data/Allen_715093703_10bins_1sampled_start-50_end250.Rda"
 
 
 labels <- "natural_scene_stimulus_id"
@@ -39,7 +38,7 @@ tictoc::tic()
 anova_results <- run_ANOVA(binned_data_name, labels)
 tictoc::toc()
 
-# save(anova_results, file = "anova_715093703_10bins_1sampled.Rda", compress = TRUE)
+save(anova_results, file = "anova_715093703_10bins_1sampled.Rda", compress = TRUE)
 
 
 # should be able to speed up the ANOVA a lot by doing it in chunks
@@ -50,59 +49,59 @@ tictoc::toc()
 
 
 
-
-# plot the results...
-
-significance_level <- .0001
-
-anova_results |> 
-  na.omit() |>   # hacky - should figure out why there are NAs but ok for quick and dirty
-  mutate(stat_sig = p_val < significance_level) |> 
-  group_by(time_period) |>
-  summarize(percent_selective = 100 * mean(stat_sig)) |>
-  mutate(Time  = NeuroDecodeR:::get_center_bin_time(time_period)) |>
-  ggplot(aes(Time, percent_selective)) +
-  geom_point() + 
-  geom_line() + 
-  ylab(paste("Percent of sites selective   (p < ", significance_level, ")")) +
-  geom_hline(yintercept = 100 * significance_level, col = "red") +
-  theme_bw()
-
-
-# facet by brain area...
-anova_results |> 
-  na.omit() |>   # hacky - should figure out why there are NAs but ok for quick and dirty
-  mutate(stat_sig = p_val < significance_level) |> 
-  group_by(time_period, site_info.ephys_structure_acronym) |>
-  summarize(percent_selective = 100 * mean(stat_sig), n = n()) |>
-  mutate(Time  = NeuroDecodeR:::get_center_bin_time(time_period)) |>
-  mutate(brain_region_name = paste0(site_info.ephys_structure_acronym, 
-                                    " (", n ,")")) |>
-  ggplot(aes(Time, percent_selective)) +
-  geom_point() + 
-  geom_line() + 
-  ylab(paste("Percent of sites selective   (p < ", significance_level, ")")) +
-  geom_hline(yintercept = 100 * significance_level, col = "red") +
-  theme_bw() + 
-  facet_wrap(~brain_region_name)
-
-
-
-# overlapping colors by brain area
-anova_results |> 
-  na.omit() |>   # hacky - should figure out why there are NAs but ok for quick and dirty
-  mutate(stat_sig = p_val < significance_level) |> 
-  group_by(time_period, site_info.ephys_structure_acronym) |>
-  summarize(percent_selective = 100 * mean(stat_sig), n = n()) |>
-  mutate(Time  = NeuroDecodeR:::get_center_bin_time(time_period)) |>
-  mutate(brain_region_name = paste0(site_info.ephys_structure_acronym, 
-                                    " (", n ,")")) |>
-  ggplot(aes(Time, percent_selective, col = brain_region_name)) +
-  geom_point() + 
-  geom_line() + 
-  ylab(paste("Percent of sites selective   (p < ", significance_level, ")")) +
-  geom_hline(yintercept = 100 * significance_level, col = "red") +
-  theme_bw() 
+# 
+# # plot the results...
+# 
+# significance_level <- .0001
+# 
+# anova_results |> 
+#   na.omit() |>   # hacky - should figure out why there are NAs but ok for quick and dirty
+#   mutate(stat_sig = p_val < significance_level) |> 
+#   group_by(time_period) |>
+#   summarize(percent_selective = 100 * mean(stat_sig)) |>
+#   mutate(Time  = NeuroDecodeR:::get_center_bin_time(time_period)) |>
+#   ggplot(aes(Time, percent_selective)) +
+#   geom_point() + 
+#   geom_line() + 
+#   ylab(paste("Percent of sites selective   (p < ", significance_level, ")")) +
+#   geom_hline(yintercept = 100 * significance_level, col = "red") +
+#   theme_bw()
+# 
+# 
+# # facet by brain area...
+# anova_results |> 
+#   na.omit() |>   # hacky - should figure out why there are NAs but ok for quick and dirty
+#   mutate(stat_sig = p_val < significance_level) |> 
+#   group_by(time_period, site_info.ephys_structure_acronym) |>
+#   summarize(percent_selective = 100 * mean(stat_sig), n = n()) |>
+#   mutate(Time  = NeuroDecodeR:::get_center_bin_time(time_period)) |>
+#   mutate(brain_region_name = paste0(site_info.ephys_structure_acronym, 
+#                                     " (", n ,")")) |>
+#   ggplot(aes(Time, percent_selective)) +
+#   geom_point() + 
+#   geom_line() + 
+#   ylab(paste("Percent of sites selective   (p < ", significance_level, ")")) +
+#   geom_hline(yintercept = 100 * significance_level, col = "red") +
+#   theme_bw() + 
+#   facet_wrap(~brain_region_name)
+# 
+# 
+# 
+# # overlapping colors by brain area
+# anova_results |> 
+#   na.omit() |>   # hacky - should figure out why there are NAs but ok for quick and dirty
+#   mutate(stat_sig = p_val < significance_level) |> 
+#   group_by(time_period, site_info.ephys_structure_acronym) |>
+#   summarize(percent_selective = 100 * mean(stat_sig), n = n()) |>
+#   mutate(Time  = NeuroDecodeR:::get_center_bin_time(time_period)) |>
+#   mutate(brain_region_name = paste0(site_info.ephys_structure_acronym, 
+#                                     " (", n ,")")) |>
+#   ggplot(aes(Time, percent_selective, col = brain_region_name)) +
+#   geom_point() + 
+#   geom_line() + 
+#   ylab(paste("Percent of sites selective   (p < ", significance_level, ")")) +
+#   geom_hline(yintercept = 100 * significance_level, col = "red") +
+#   theme_bw() 
 
 
 
