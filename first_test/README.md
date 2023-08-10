8238009cafe1f6726d53d9016e471b976e7fc2a5
Ethan Meyers <em939@grace1.grace.hpc.yale.internal>
2023-08-10 16:53:48 -0400

    Updating all files before refactoring code

--- first_test/README.md
+++
@@ -1,1 +0,0 @@
-# Testing William's cluster code
--- first_test/results/r_scripts/analysis.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -i
-#SBATCH -J 10neurons
-#SBATCH -c 16
-#SBATCH -p bigmem
-#SBATCH --constraint cascadelake
-#SBATCH --mem=1500G
-module load R/4.1.0-foss-2020b
-Rscript Pending/"test_william_run_ANOVA_try_06.R"
